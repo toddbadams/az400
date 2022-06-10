ea02f06a36f70ec153d067a56bfa4520ba28bdbd
todd adams <toddbadams@msn.com>
2022-06-10 09:24:29 +0100

    wip

---
+++ examples/terraform/main.tf
@@ -0,0 +1,36 @@
+resource "azurerm_resource_group" "rg" {
+  name     = "tba-rg-time-to-text-uks"
+  location = "West Europe"
+}
+
+resource "azurerm_storage_account" "sa" {
+  name                     = "functionsapptestsa"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_app_service_plan" "asp" {
+  name                = "azure-functions-test-service-plan"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  kind                = "Linux"
+  reserved            = true
+
+  sku {
+    tier = "Dynamic"
+    size = "Y1"
+  }
+}
+
+resource "azurerm_function_app" "fa" {
+  name                       = "test-azure-functions"
+  location                   = azurerm_resource_group.rg.location
+  resource_group_name        = azurerm_resource_group.rg.name
+  app_service_plan_id        = azurerm_app_service_plan.asp.id
+  storage_account_name       = azurerm_storage_account.sa.name
+  storage_account_access_key = azurerm_storage_account.sa.primary_access_key
+  os_type                    = "linux"
+  version                    = "~3"
+}
